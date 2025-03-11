97780e2cecc4f80222e18ffce5fcfcc1a54ae367
Gaeun Lee <rk243812@gmail.com>
2025-03-11 22:24:54 +0900

    Title: 데이터 비교, Time: 480ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 250311/데이터 비교/data-comparison.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.HashSet;
+import java.lang.StringBuilder;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int n1 = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int[] arr1 = new int[n1];
+        HashSet<Integer> hs1 = new HashSet<>();
+        for (int i = 0; i < n1; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            arr1[i] = num;
+            hs1.add(num);
+        }
+
+        int n2 = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int[] arr2 = new int[n2];
+        HashSet<Integer> hs2 = new HashSet<>();
+        for (int i = 0; i < n2; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            arr2[i] = num;
+            hs2.add(num);
+        }
+
+        for (int elem : arr2) {
+            if (hs1.contains(elem)) {
+                sb.append(1);
+            } else {
+                sb.append(0);
+            }
+            sb.append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
