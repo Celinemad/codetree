7e09d0f98d5e3ea1eb4c7ecb451ca678eb5d0f1a
Gaeun Lee <rk243812@gmail.com>
2025-03-12 16:27:10 +0900

    Title: 친한 점, Time: 88ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250312/친한 점/frendly-point.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+    }
+}
