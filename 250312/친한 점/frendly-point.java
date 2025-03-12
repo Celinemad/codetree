2850bf3f0308e3d122a562712ba31dd340cd65f3
Gaeun Lee <rk243812@gmail.com>
2025-03-12 16:57:33 +0900

    Title: 친한 점, Time: 362ms, Memory: 23MB, Status: Passed - CodeTree

--- 250312/친한 점/frendly-point.java
+++ 250312/친한 점/frendly-point.java
@@ -1,9 +1,60 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+import java.lang.StringBuilder;
+
+class Point implements Comparable<Point> {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point p) {
+        if (this.x != p.x) 
+            return this.x - p.x;
+        else 
+            return this.y - p.y;
+    }
+}
 
 public class Main {
-    public static void main(String[] args) {
-        // Please write your code here.
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        TreeSet<Point> ts = new TreeSet<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            ts.add(new Point(x, y));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            Point result = ts.ceiling(new Point(x, y));
+            if (result == null) {
+                sb.append(-1).append(" ").append(-1);
+            } else {
+                sb.append(result.x).append(" ").append(result.y);
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
     }
 }
