f531aa6a2cc71bffaadbfa5da72a3e6279819634
Gaeun Lee <rk243812@gmail.com>
2025-03-13 19:45:59 +0900

    Title: 최소 정수 출력, Time: 424ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 250313/최소 정수 출력/min-integer-output.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.lang.StringBuilder;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0);
+                } else {
+                    sb.append(pq.poll());
+                }
+                sb.append("\n");
+            } else {
+                pq.add(num);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
