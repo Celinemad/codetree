817d403a0403c9e75f5d966590a2816d542d332a
Gaeun Lee <rk243812@gmail.com>
2025-03-19 15:08:01 +0900

    Title: 서로 다른 BST 개수 세기, Time: 95ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250319/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int MAX_N = 19;
+    static int[] dp = new int[MAX_N + 1];
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        N = Integer.parseInt(br.readLine());
+
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i-1] * 2;
+            for (int j = 1; j <= i-2; j++) {
+                dp[i] += dp[j] * dp[i-j-1];
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
