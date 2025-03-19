3d6eb874c625253eb4bd025027466de81773ab6b
Gaeun Lee <rk243812@gmail.com>
2025-03-19 15:11:00 +0900

    Title: 서로 다른 BST 개수 세기, Time: 91ms, Memory: 9MB, Status: Runtime Error - CodeTree

--- 250319/서로 다른 BST 개수 세기/number-of-unique-bst.java
+++ 250319/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -3,26 +3,26 @@ import java.io.*;
 
 public class Main {
 
-    static int MAX_N = 19;
-    static int[] dp = new int[MAX_N + 1];
-    static int N;
+    // static int MAX_N = 19;
+    // static int[] dp = new int[MAX_N + 1];
+    // static int N;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
+        int[] dp = new int[N + 1];
+        
         dp[0] = 1;
         dp[1] = 1;
         dp[2] = 2;
 
-        int sum;
         for (int i = 3; i <= N; i++) {
-            sum = dp[i-1] * 2;
+            dp[i] = dp[i-1] * 2;
             for (int j = 1; j <= i-2; j++) {
-                sum += dp[j] * dp[i-j-1];
+                dp[i] += dp[j] * dp[i-j-1];
             }
-            dp[i] = sum;
         }
         System.out.println(dp[N]);
     }
