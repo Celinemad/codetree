2f64f0740437d06a65a295754638c87f06d4dc77
Gaeun Lee <rk243812@gmail.com>
2025-03-20 15:54:24 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 3078ms, Memory: 23MB, Status: Time Limit Exceed - CodeTree

--- 250320/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 250320/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -7,59 +7,54 @@ public class Main {
     static int[][] grid = new int[MAX_N][MAX_N];
     static int[][] dp = new int[MAX_N][MAX_N];
 
-    static class Pair implements Comparable<Pair> {
-        int r, c;
-        int num;
-
-        public Pair(int r, int c, int num) {
-            this.r = r;
-            this.c = c;
-            this.num = num;
+    static int[] drs = new int[] {0, 0, 1, -1};
+    static int[] dcs = new int[] {1, -1, 0, 0};
+    static int findMax(int r, int c) {
+        if (dp[r][c] != -1) {
+            return dp[r][c];
         }
 
-        @Override
-        public int compareTo(Pair p) {
-            return this.num - p.num;
+        int best = 1;
+        int newR, newC;
+        for (int i = 0; i < 4; i++) {
+            newR = r + drs[i];
+            newC = c + dcs[i];
+            if (inRange(newR, newC) && grid[newR][newC] < grid[r][c]) {
+                best = Math.max(best, findMax(newR, newC) + 1);
+            }
         }
+
+        return best;
     }
 
-    public static boolean inRange(int r, int c) {
+    static boolean inRange(int r, int c) {
         return 0 <= r && r < N && 0 <= c && c < N;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         N = Integer.parseInt(br.readLine());
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-
+        
         StringTokenizer st;
-        int num;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                num =  Integer.parseInt(st.nextToken());
-                grid[i][j] = num;
-                dp[i][j] = 1;
-                pq.add(new Pair(i, j, num));
+                grid[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int[] drs = new int[] {0, 0, 1, -1};
-        int[] dcs = new int[] {1, -1, 0, 0};
-        int newR, newC;
-        while (!pq.isEmpty()) {
-            Pair p = pq.poll();
-
-            for (int i = 0; i < 4; i++) {
-                newR = p.r + drs[i];
-                newC = p.c + dcs[i];
-                if (inRange(newR, newC) && grid[p.r][p.c] < grid[newR][newC]) {
-                    dp[newR][newC] = Math.max(dp[newR][newC], dp[p.r][p.c] + 1);
-                }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                dp[i][j] = -1;
             }
-        } 
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                dp[i][j] = findMax(i, j);
+            }
+        }
 
         int maxLen = Integer.MIN_VALUE;
         for (int i = 0; i < N; i++) {
@@ -69,6 +64,5 @@ public class Main {
         }
 
         System.out.println(maxLen);
-        
     }
 }
