29d3727c95fe9b1b45aa3d98565f79dc7b2baf02
Gaeun Lee <rk243812@gmail.com>
2025-03-26 15:51:17 +0900

    Title: 최대 증가 부분 수열, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

--- 250326/최대 증가 부분 수열/longest-increasing-subsequence.java
+++ 250326/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -17,9 +17,9 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
         }
 
-        dp[0] = 1;
         for (int i = 1; i < N; i++) {
             for (int j = 0; j < i; j++) {
                 if (arr[j] < arr[i]) {
